c91a501d5276a6417e16ae4b0abfb00622701712
vedant <vedantwagh1990@mail.com>
2026-06-08 18:45:05 +0530

    updatwe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to chandra"); 
+    res.send("Welcome to chandra in cbz"); 
 });
 
 app.get("/health", (req, res) => {
