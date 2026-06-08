2506bf859a69ee929b9b1bb531bfa8c7d1571479
vedant <vedantwagh1990@mail.com>
2026-06-08 18:43:38 +0530

    save and update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to cbz-nodejs-app"); 
+    res.send("Welcome to chandra"); 
 });
 
 app.get("/health", (req, res) => {
